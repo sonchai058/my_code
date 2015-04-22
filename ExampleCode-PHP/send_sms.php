04f1be17da20730fa25073fd76db73dfea6cbab6
sonchai058 <sonchai058@gmail.com>
2015-04-22 15:24:13 +0700

    กเดกด้เด
    ้ดเ้ดเ้ดเ

--- ExampleCode-PHP/send_sms.php
+++ ExampleCode-PHP/send_sms.php
@@ -8,7 +8,7 @@
 	$sender = $_REQUEST['sender'];
 	$ScheduledDelivery = $_REQUEST['ScheduledDelivery'];
 	$force = $_REQUEST['force'];
-	ดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดด
+	ดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดดด11111111111111111111111
 	$result = sms::send_sms($username,$password,$msisdn,$message,$sender,$ScheduledDelivery,$force);
 	echo $result;
 ?>
